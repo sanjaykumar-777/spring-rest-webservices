656216f4dcd338856e8d3613a76de6c5ea6fa890
sanjay-kumar777 <sanjaykumar7m7@gmail.com>
2024-08-22 22:27:35 +0530

    get-mapping

--- src/main/java/com/sanjaykumar_777/springboot_rest_webservices/helloworld/HelloWorldController.java
+++ src/main/java/com/sanjaykumar_777/springboot_rest_webservices/helloworld/HelloWorldController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping(path = "/hello-world")
     public String helloWorld(){
         return "hello world";
     }
