392841e4e0d12d150b4376e897f41762ef544c06
sanjay-kumar777 <sanjaykumar7m7@gmail.com>
2024-08-22 22:34:54 +0530

    return a hello world bean

---
+++ src/main/java/com/sanjaykumar_777/springboot_rest_webservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,23 @@
+package com.sanjaykumar_777.springboot_rest_webservices.helloworld;
+
+public class HelloWorldBean {
+    private String message;
+    public HelloWorldBean(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/sanjaykumar_777/springboot_rest_webservices/helloworld/HelloWorldController.java
+++ src/main/java/com/sanjaykumar_777/springboot_rest_webservices/helloworld/HelloWorldController.java
@@ -12,4 +12,9 @@ public class HelloWorldController {
     public String helloWorld(){
         return "hello world";
     }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
 }
