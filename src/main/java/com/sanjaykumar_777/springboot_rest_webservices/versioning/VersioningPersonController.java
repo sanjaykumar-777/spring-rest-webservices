0a82d032d41f71c1549a248e3063f8bf78eee064
sanjay-kumar777 <sanjaykumar7m7@gmail.com>
2024-08-25 19:46:43 +0530

    custom header versioning

--- src/main/java/com/sanjaykumar_777/springboot_rest_webservices/versioning/VersioningPersonController.java
+++ src/main/java/com/sanjaykumar_777/springboot_rest_webservices/versioning/VersioningPersonController.java
@@ -18,26 +18,40 @@ public class VersioningPersonController {
     URI versioning
      */
     @GetMapping("/v1/person")
-    public PersonV1 getFirstVersionOfPerson(){
+    public PersonV1 getFirstVersionOfPerson() {
         return new PersonV1("Bob charlie");
     }
+
     @GetMapping("/v2/person")
-    public PersonV2 getSecondVersionOfPerson(){
-        return new PersonV2(new Name("Bob","Marlie"));
+    public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2(new Name("Bob", "Marlie"));
     }
 
     /*
     Parameter versioning
      */
-
     @GetMapping(path = "/person", params = "version=1")
-    public PersonV1 getFirstVersionOfPersonRequestParameter(){
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
         return new PersonV1("Bob marlie");
     }
 
-    @GetMapping(path = "/person",params = "version=2")
-    public PersonV2 getSecondVersionOfPersonRequestParameter(){
-        return new PersonV2(new Name("Bob","Marlie"));
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter() {
+        return new PersonV2(new Name("Bob", "Marlie"));
+    }
+
+    /*
+    custom header versioning
+     */
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader() {
+        return new PersonV1("Bob Marlie");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeader() {
+        return new PersonV2(new Name("Bob", "Marlie"));
     }
 
 
