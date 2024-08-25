5942981691d725276e44940c682444a3724ec543
sanjay-kumar777 <sanjaykumar7m7@gmail.com>
2024-08-25 19:34:26 +0530

    paramter versioning

--- src/main/java/com/sanjaykumar_777/springboot_rest_webservices/versioning/VersioningPersonController.java
+++ src/main/java/com/sanjaykumar_777/springboot_rest_webservices/versioning/VersioningPersonController.java
@@ -6,13 +6,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersioningPersonController {
 
+    /*
+     * Versioning types
+     * 1.URI versioning
+     * 2.Parameter versioning
+     * 3.custom headers versioning
+     * 4.content negotiation
+     */
+
+    /*
+    URI versioning
+     */
     @GetMapping("/v1/person")
     public PersonV1 getFirstVersionOfPerson(){
         return new PersonV1("Bob charlie");
     }
-
     @GetMapping("/v2/person")
     public PersonV2 getSecondVersionOfPerson(){
         return new PersonV2(new Name("Bob","Marlie"));
     }
+
+    /*
+    Parameter versioning
+     */
+
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter(){
+        return new PersonV1("Bob marlie");
+    }
+
+    @GetMapping(path = "/person",params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter(){
+        return new PersonV2(new Name("Bob","Marlie"));
+    }
+
+
 }
