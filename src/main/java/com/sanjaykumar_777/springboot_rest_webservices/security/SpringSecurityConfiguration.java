6793f9efb87788b52ba825a92c08cc608669fd07
sanjay-kumar777 <sanjaykumar7m7@gmail.com>
2024-08-28 23:28:29 +0530

    spring security and mysql database connection

---
+++ src/main/java/com/sanjaykumar_777/springboot_rest_webservices/security/SpringSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.sanjaykumar_777.springboot_rest_webservices.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //1. All requests should be authenticated
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        //2. If a request is not authenticated, a web page is shown
+        http.httpBasic(Customizer.withDefaults());
+        //3 . remove CSRF(Cross-Site Request Forgery) - POST, PUT
+        //csrf is currently depricated
+        http.csrf().disable();
+        return http.build();
+
+    }
+}
